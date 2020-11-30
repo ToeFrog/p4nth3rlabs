d45342ae5c0280515f5ee635edd5bde856067caa
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2020-11-30 19:45:54 +0000

    :bug: Removed unused var

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useReducer, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Socket from './socket';
 import AppContext, { AppState } from './AppContext';
 import AppReducer from './AppReducer';
