b9480f3b163473cd9866a3ad631e536967d4c31c
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2020-12-09 11:33:47 +0000

    :lipstick: Optimised animations

--- src/components/overlay/index.style.ts
+++ src/components/overlay/index.style.ts
@@ -1,4 +1,16 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
+
+const textBoxWidths = {
+  github: '400px',
+  twitter: '276px',
+  discord: '380px',
+  slogan: '432px',
+};
+
+const fadeIn = keyframes`
+  from { opacity: 0; }
+  to { opacity: 1; }
+}`;
 
 const FooterContainer = styled.div`
   display: block;
@@ -8,25 +20,112 @@ const FooterContainer = styled.div`
   height: 100vh;
 `;
 
-const TextBox = styled.div`
-  background-color: var(--black);
-  border-radius: 8px;
+interface TextBoxProps {
+  type: string;
+}
+
+function getWidthFromType(props: TextBoxProps) {
+  const { type } = props;
+
+  switch (type) {
+    case 'twitter':
+      return textBoxWidths.twitter;
+    case 'discord':
+      return textBoxWidths.discord;
+    case 'github':
+      return textBoxWidths.github;
+    case 'slogan':
+      return textBoxWidths.slogan;
+  }
+}
+
+function getGrowAnimation(props: TextBoxProps) {
+  const { type } = props;
+  switch (type) {
+    case 'twitter':
+      return keyframes`
+      from { width: 0; }
+      to { width: ${textBoxWidths.twitter}; }
+    `;
+    case 'discord':
+      return keyframes`
+      from { width: 0; }
+      to { width: ${textBoxWidths.discord}; }
+    `;
+    case 'github':
+      return keyframes`
+      from { width: 0; }
+      to { width: ${textBoxWidths.github}; }
+    `;
+    case 'slogan':
+      return keyframes`
+      from { width: 0; }
+      to { width: ${textBoxWidths.slogan}; }
+    `;
+  }
+}
+
+const textBoxHeight = '36px';
+const textBoxBorderRadius = '8px';
+
+const TextBoxContainer = styled.div`
   position: relative;
-  padding: 4px 16px;
-  display: inline-flex;
-  justify-content: center;
+  display: flex;
   align-items: center;
-  position: relative;
+  height: ${textBoxHeight};
   left: 1205px;
-  top: 1012px;
+  top: 1002px;
+`;
+
+const TextBox = styled.div<TextBoxProps>`
+  animation: ${(props) => getGrowAnimation(props)} 1s ease-in-out;
+  animation-fill-mode: forwards;
+  animation-iteration-count: 1;
+  overflow: hidden;
+  background-color: var(--black);
+  position: relative;
+  display: inline-flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: ${textBoxHeight};
+  width: ${(props) => getWidthFromType(props)};
+`;
+
+const TextBoxLeft = styled.span`
+  width: ${textBoxBorderRadius};
+  height: 100%;
+  display: inline-flex;
+  justify-content: flex-start;
+  align-items: center;
+  border-radius: ${textBoxBorderRadius} 0 0 ${textBoxBorderRadius};
+  background-color: var(--black);
+`;
+const TextBoxRight = styled.span`
+  width: ${textBoxBorderRadius};
+  height: 100%;
+  display: inline-flex;
+  justify-content: flex-start;
+  align-items: center;
+  border-radius: 0 ${textBoxBorderRadius} ${textBoxBorderRadius} 0;
+  background-color: var(--black);
+  position: relative;
+  left: -1px;
 `;
 
 const TextBoxText = styled.span`
+  animation: ${fadeIn} 3s ease-in-out;
+  animation-fill-mode: forwards;
+  animation-iteration-count: 1;
+  animation-delay: 1s;
+  overflow: hidden;
+  opacity: 0;
+  white-space: nowrap;
   color: var(--white);
   font-family: var(--font-family-main);
   font-size: 18px;
   text-transform: uppercase;
   font-weight: bold;
+  width: 100%;
 `;
 
-export { FooterContainer, TextBox, TextBoxText };
+export { FooterContainer, TextBox, TextBoxText, TextBoxLeft, TextBoxRight, TextBoxContainer };
--- src/components/overlay/index.tsx
+++ src/components/overlay/index.tsx
@@ -1,26 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import Icon from './icon';
-import { FooterContainer, TextBox, TextBoxText } from './index.style';
+import {
+  FooterContainer,
+  TextBox,
+  TextBoxText,
+  TextBoxLeft,
+  TextBoxRight,
+  TextBoxContainer,
+} from './index.style';
 
 interface OverlayProps {}
 
 interface TextBoxOption {
   text: string;
   iconName: string;
+  type: string;
 }
 
 const textBoxOptions: TextBoxOption[] = [
   {
-    text: 'Build stuff, learn things, love what you do.',
+    text: 'Build stuff, learn things, love what you do',
     iconName: 'lightening',
+    type: 'slogan',
   },
   {
     text: '!twitter - @whitep4nth3r',
     iconName: 'twitter',
+    type: 'twitter',
   },
   {
-    text: '!discord - Join our Discord community, The Claw',
+    text: '!discord - Join our Discord community',
     iconName: 'discord',
+    type: 'discord',
+  },
+  {
+    text: '!github - Find stream projects on github',
+    iconName: 'github',
+    type: 'github',
   },
 ];
 
@@ -34,16 +50,20 @@ export default function Overlay(props: OverlayProps) {
         textBoxCurrentKey + 1 > textBoxOptions.length - 1 ? 0 : textBoxCurrentKey + 1,
       );
       setTextBox(textBoxOptions[textBoxCurrentKey]);
-    }, 2000);
+    }, 30000);
     return () => clearInterval(interval);
   }, [textBoxCurrentKey]);
 
   return (
     <FooterContainer>
-      <TextBox>
-        <Icon iconName={textBox.iconName}></Icon>
-        <TextBoxText>{textBox.text}</TextBoxText>
-      </TextBox>
+      <TextBoxContainer>
+        <TextBoxLeft />
+        <TextBox key={`item-${textBoxCurrentKey}`} type={textBox.type}>
+          <Icon iconName={textBox.iconName}></Icon>
+          <TextBoxText>{textBox.text}</TextBoxText>
+        </TextBox>
+        <TextBoxRight />
+      </TextBoxContainer>
     </FooterContainer>
   );
 }
