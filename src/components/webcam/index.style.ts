df9679ebc1e290013276d56e837c6a899a2d13d8
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-01-10 15:20:56 +0000

    More webcam frame fixes

--- src/components/webcam/index.style.ts
+++ src/components/webcam/index.style.ts
@@ -4,8 +4,8 @@ import { motion } from 'framer-motion';
 export const WebcamPosProps = {
   pantherOffset: -56,
   pantherAdjust: 18,
-  height: 1080,
-  width: 1920,
+  height: 1013,
+  width: 1800,
   borderWidth: 24,
 };
 
@@ -15,33 +15,27 @@ const Panther = styled(motion.img)`
   bottom: ${WebcamPosProps.pantherOffset}px;
 `;
 
-const highlight = keyframes`
-  100% {
-    background-position: 0 0, 0 0;
-  }
+const WebcamContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 `;
 
-const WebcamContainer = styled.div`
-  background: transparent;
+const WebcamFrame = styled.div`
   --border-width: ${WebcamPosProps.borderWidth}px;
-
-  display: block;
+  background: transparent;
+  display: flex;
+  justify-self: center;
+  align-self: center;
   height: ${WebcamPosProps.height}px;
   width: ${WebcamPosProps.width}px;
   box-sizing: border-box;
   position: relative;
   border-width: var(--border-width);
   border-style: solid;
-  border-color: transparent;
-  background-repeat: no-repeat;
-  background-origin: padding-box, border-box;
-  background-image: linear-gradient(white, white),
-    linear-gradient(45deg, var(--vip), var(--yellow) 50%, var(--red));
-  background-repeat: no-repeat;
-  background-size: 100% 100%, 100% 200%;
-  background-position: 0 0, 0 100%;
-  background-origin: padding-box, border-box;
-  animation: ${highlight} 10s infinite alternate;
+  border-image-slice: 1;
+  border-image-source: linear-gradient(45deg, var(--yellow) 0%, var(--vip) 100%);
 `;
 
-export { WebcamContainer, Panther };
+export { WebcamContainer, WebcamFrame, Panther };
